2b19ea76f8599f5372aacbe2d0f7a3673ce1b4ea
zizhou <hangli_work@163.com>
2024-12-17 12:24:22 +0800

    update

--- algo/src/classical150/Connect.java
+++ algo/src/classical150/Connect.java
@@ -1,6 +1,91 @@
-package classical150;/** 
-@author zizhou 
-@version 1.0.0
-@date 2024-12-17 11:16
-*/public class Connect {
+package classical150;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author zizhou
+ * @version 1.0.0
+ * @date 2024-12-17 11:16
+ */
+public class Connect {
+
+    /**
+     * 遍历当前层时，就为下一层构建next指针
+     *
+     * @param root
+     * @return
+     */
+    public Node connect(Node root) {
+        if (root == null) return null;
+
+        Node cur = root;
+        while (cur != null) {
+            // 下一层虚拟头结点
+            Node dummy = new Node(0);
+            // 用于构建下一层的链表
+            Node p = dummy;
+            // 使用当前层所有的节点，构建下一层
+            while (cur != null) {
+                if (cur.left != null) {
+                    p.next = cur.left;
+                    p = p.next;
+                }
+                if (cur.right != null) {
+                    p.next = cur.right;
+                    p = p.next;
+                }
+                cur = cur.next;
+            }
+            // 下一层构建完毕
+
+            // 考察下一层
+            cur = dummy.next;
+        }
+        return root;
+    }
+
+    public Node connect0(Node root) {
+        if (root == null) return null;
+        Deque<Node> queue = new ArrayDeque<>();
+
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            Node last = null;
+            for (int i = 1; i <= n; i++) {
+                Node node = queue.poll();
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+                if (i != 1) {
+                    last.next = node;
+                }
+                last = node;
+            }
+        }
+        return root;
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    ;
 }
--- algo/src/heat100/Flatten.java
+++ algo/src/heat100/Flatten.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 /**
  * 给你二叉树的根结点 root ，请你将它展开为一个单链表
  * - 展开后的单链表应该同样使用 TreeNode ，
- *   其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+ * 其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
  * - 展开后的单链表应该与二叉树 先序遍历 顺序相同。
  *
  * @author zizhou
@@ -16,7 +16,7 @@ import java.util.ArrayList;
  * @date 2024-11-06 9:22
  */
 public class Flatten {
-   ArrayList<TreeNode> treeNodes = new ArrayList<>();
+    ArrayList<TreeNode> treeNodes = new ArrayList<>();
 
     public static void main(String[] args) {
         BTreeUtils utils = new BTreeUtils();
@@ -26,15 +26,39 @@ public class Flatten {
         utils.printByLevelOrder(root);
     }
 
+    /**
+     * 原地算法（着重处理的是right）
+     * - 前序遍历时，root,left,right
+     * - 对于本次遍历，cur_root, cur_left已经遍历完毕，但是cur_right，要等last_right遍历结束，而last_right也是next_root的前驱节点
+     * - 找到last_right后，令last_right -> cur.right, cur_right -> next_left
+     *
+     * @param root
+     */
+    public void flatten2(TreeNode root) {
+        if (root == null) return;
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left != null) {
+                TreeNode next = cur.left;
+                TreeNode prev = next;
+                while (prev.right != null) prev = prev.right;
+                prev.right = cur.right;
+                cur.left = null;
+                cur.right = next;
+            }
+            cur = cur.right;
+        }
+    }
+
     public void flatten(TreeNode root) {
         preorder(root);
         for (int i = 0; i < treeNodes.size() - 1; i++) {
             treeNodes.get(i).left = null;
-            treeNodes.get(i).right = treeNodes.get(i+1);
+            treeNodes.get(i).right = treeNodes.get(i + 1);
         }
     }
 
-    public void preorder(TreeNode root){
+    public void preorder(TreeNode root) {
         if (root == null) return;
         treeNodes.add(root);
         preorder(root.left);
